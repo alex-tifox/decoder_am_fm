db4648eb2ba0ec78de450ec0057da9f1e5eb6a5c
alex.tifox <tilnyi.olexander@gmail.com>
2020-06-20 22:21:55 +0200

    signal analysis - spectrum, spectrogram

---
+++ src/signal_analysis.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+
+def data_for_spectrum(data, fs):
+    fft_res = np.fft.fft(data)
+    wave_freqs = np.fft.fftfreq(len(fft_res)) * fs
+
+    return fft_res, wave_freqs
+
+
+def spectrum(data, fs):
+    fft_res, wave_freqs = data_for_spectrum(data, fs)
+
+    plt.subplot(1, 2, 1)
+    plt.plot(wave_freqs[:int(len(wave_freqs) / 2)], 20 * np.log10(fft_res)[:int(len(fft_res) / 2)])
+    plt.ylabel("Power [dB]")
+    plt.xlabel("Frequency [Hz]")
+    plt.title("Signal spectrum")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(np.linspace(0, len(data) / fs, len(data)), data)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time [s]")
+    plt.title("Signal")
+
+
+def spectrogram(yaxis):
+    my = np.array(yaxis)
+    f, t, Sxx = signal.spectrogram(my, 44100)
+    plt.pcolormesh(t, f, Sxx)
+    plt.xlabel('Time [Sec]')
+    plt.ylabel('Frequency [Hz]')
+    plt.title('Signal spectrogram')
+    plt.show()
+
+    return f, t, Sxx
