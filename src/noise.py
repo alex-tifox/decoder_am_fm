e454faf2e12776dfb68b960b16ba0bdc23c303e1
alex.tifox <tilnyi.olexander@gmail.com>
2020-06-20 22:21:32 +0200

    add noise to signal functionality

--- src/noise.py
+++ src/noise.py
@@ -1,7 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 import file_manipulation
+from signal_analysis import spectrum
 
 
 def get_white_noise():
     noise_data, samplerate = file_manipulation.read_wav_from_file(filename='noises/white.wav')
 
     return noise_data
+
+
+def add_noise_to_signal(signal, samplerate):
+    alpha_coeff = 0.5
+    noise = get_white_noise()
+
+    noised_signal = np.copy(signal)
+
+    for i in range(len(signal)):
+        noised_signal[i] = signal[i] * alpha_coeff + (noise[i % samplerate] * (1-alpha_coeff))
+
+    spectrum(noised_signal, samplerate)
+    plt.show()
+
+    return noised_signal
